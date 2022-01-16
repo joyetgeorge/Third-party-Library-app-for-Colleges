0aeb9685f63a028579d3df5b077d8b0dc37c08e2
joyetgeorge <joyetgeorge@gmail.com>
2022-01-16 18:52:24 +0530

    sbin search

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,106 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(BookImage());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BookImage extends StatefulWidget {
+  BookImage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _BookImageState createState() => _BookImageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BookImageState extends State<BookImage> {
+  final Controller = TextEditingController();
 
-  void _incrementCounter() {
+  String sbin = '';
+  Map mapResponse = {};
+  bool fetch = false;
+
+  Future fetchPost() async {
+    http.Response response = await http.get(
+        Uri.parse('https://www.googleapis.com/books/v1/volumes?q=isbn:$sbin'));
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      mapResponse = json.decode(response.body);
+      // print(mapResponse.toString());
     });
   }
 
   @override
+  void initState() {
+    fetchPost();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                      top: 100, bottom: 100, left: 50, right: 50),
+                  child: TextField(
+                    controller: Controller,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter SBIN',
+                      hintText: 'SBIN',
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              fetch = true;
+                              sbin = Controller.text;
+                            });
+                            fetchPost();
+                          },
+                          icon: Icon(Icons.send)),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 400,
+                  margin: EdgeInsets.only(bottom: 10),
+                  child: Image.network(
+                      'https://covers.openlibrary.org/b/isbn/$sbin-L.jpg'),
+                ),
+                fetch == true
+                    ? Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            child: Text(
+                              mapResponse['items'][0]['volumeInfo']['title']
+                                  .toString(),
+                              style: GoogleFonts.poppins(fontSize: 30),
+                            ),
+                          ),
+                          Container(
+                            child: Text(
+                              mapResponse['items'][0]['volumeInfo']
+                                      ['description']
+                                  .toString(),
+                              style: GoogleFonts.poppins(fontSize: 20),
+                            ),
+                            margin: EdgeInsets.only(left: 50, right: 50),
+                          ),
+                        ],
+                      )
+                    : Text(''),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      debugShowCheckedModeBanner: false,
     );
   }
 }
